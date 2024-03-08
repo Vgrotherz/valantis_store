5a250cad3c00465362f2792aa2d1ce4b87002ed0
Vgrotherz <vgrotherz@gmail.com>
2024-03-08 23:23:46 +0700

    testing proxy

--- src/components/valantisApp/valantisApp.js
+++ src/components/valantisApp/valantisApp.js
@@ -1,6 +1,6 @@
 import md5 from 'md5';
-const proxy = 'https://cors-anywhere.herokuapp.com/'
-const apiUrl = `${proxy}http://api.valantis.store:40000/`; // добавлен прокси для gh pages для устранения конфликта https и http
+
+const apiUrl = `http://api.valantis.store:40000/`; // добавлен прокси для gh pages для устранения конфликта https и http
 const apiPassword = 'Valantis';
 const retryTime = 1000; // время после которого будет повтор получения данных
 
