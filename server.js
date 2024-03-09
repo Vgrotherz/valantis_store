3b647bdcb5cb1e90d53b4a4dd86011875a7f2470
Vgrotherz <vgrotherz@gmail.com>
2024-03-09 23:31:40 +0700

    move server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files from the '../../../build' directory of your React application
-app.use(express.static(path.join(__dirname, '../../../build')));
+app.use(express.static(path.join(__dirname, './build')));
 
 // Route handler for the root URL
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../../build', 'index.html'));
+  res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
